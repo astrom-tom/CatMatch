e46e9a958168c9e2d418c16ac56da17eadb4e141
romain <the.spartan.proj@gmail.com>
2019-03-04 14:43:51 -0300

    include catscii

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
-import catmatch.catmatch as cats
+import catmatch.__main__ as cats
 
 setup(
    name = 'catmatch',
    version = cats.__version__,
    author = cats.__credits__,
    packages = ['catmatch'],
-   entry_points = {'gui_scripts': ['spartan = spartan.__main__:main',],},
+   entry_points = {'gui_scripts': ['catmatch = catmatch.__main__:main',],},
    description = 'A simple catalog matching script',
    python_requires = '>=3.6',
    install_requires = [
        "numpy >= 1.16",
-       "catscii>= 1.0",
+       "catscii>= 1.1",
    ],
    include_package_data=True,
 )
